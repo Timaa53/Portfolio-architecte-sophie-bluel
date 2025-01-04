5ea636dcf214c231fc0de9d279bfa48d6964ceb1
Andy Ctn <cortinandy@gmail.com>
2025-01-04 02:58:01 +0100

    tri terminé, débuts login page

--- FrontEnd/assets/js/works.js
+++ FrontEnd/assets/js/works.js
@@ -1,39 +1,74 @@
+// Fonctions
+//Création de figures
+export function creerFigure(work) {
+    let figure = document.createElement("figure");
+    let img = document.createElement("img");
+    img.src = work.imageUrl;
+    img.alt = work.title;
+    figure.appendChild(img);
+
+    let title = document.createElement("figcaption");
+    title.innerText = work.title;
+    figure.appendChild(title);
+
+    containerGallery.appendChild(figure);
+    return figure
+}
+
 // Récupération dynamique des travaux et affichage auto dans <div class="gallery">
+let works = [];
+const containerGallery = document.querySelector(".gallery");
+
 fetch('http://localhost:5678/api/works', {
     method: 'GET',
     headers: {"Content-Type": "application/json"},
 })
     .then(response => response.json())
     .then((data) => {
-        let containerGallery = document.querySelector(".gallery")
-    for (work of data){
-        let figure = document.createElement("figure")
+        works = data;
 
-        let img = document.createElement("img")
-        img.src = work.imageUrl
-        img.alt = work.title
-        figure.appendChild(img)
-
-        let title = document.createElement("figcaption")
-        title.innerText = work.title
-        figure.appendChild(title)
-
-        console.log(figure)
-        containerGallery.appendChild(figure);
+    for (let work of data){
+        containerGallery.appendChild(creerFigure(work));
     }
     })
     .catch(error => console.error(error));
 
 
-// Ecouteurs d'évènements des boutons pour styles CSS au click
+// Ecouteurs d'évènements au click des boutons de la nav btn-filters
 const buttons = document.querySelectorAll(".btn-filters button");
 
-buttons.forEach(button => {
+for (const button of buttons) {
     button.addEventListener("click", () => {
-        buttons.forEach(btn => btn.classList.remove("active"));
-        
+        // Modification dynamique de styles des boutons
+        for (const btn of buttons) {
+            btn.classList.remove("active");
+        }
         button.classList.add("active");
-    });
-});
 
-// Ecouteurs d'évènements des boutons pour tri catégories au click
+        containerGallery.innerHTML = "";
+
+        if (button.classList.contains("all-works")) {
+            for (let work of works){
+                containerGallery.appendChild(creerFigure(work));
+            }
+        };
+        if (button.classList.contains("objets-btn")) {
+            const objetsFilter = works.filter(work => work.category.name === "Objets");
+            for (const work of objetsFilter) {
+                containerGallery.appendChild(creerFigure(work));
+            }
+        };
+        if (button.classList.contains("appartements-btn")) {
+            const appartementsFilter = works.filter(work => work.category.name === "Appartements");
+            for (const work of appartementsFilter) {
+                containerGallery.appendChild(creerFigure(work));
+            }
+        }; 
+        if (button.classList.contains("hotels-restaurants-btn")) {
+            const hotelsRestaurantsFilter = works.filter(work => work.category.name === "Hotels & restaurants");
+            for (const work of hotelsRestaurantsFilter) {
+                containerGallery.appendChild(creerFigure(work));
+            }
+        };
+    });
+};
